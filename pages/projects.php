d356b5749f91c6b5bb74467c851a8fbc60987746
Alphison <alfismusin13@gmail.com>
2022-09-13 00:31:35 +0300

    add projects page

---
+++ pages/projects.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700;800&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="projects">
+        <div class="projects-header">
+            <div class="projects-header__column">
+                <label class="projects-header__column__inner">
+                    <p class="filter">
+                        Фильтр:
+                    </p>
+                </label>
+                <label class="projects-header__column__inner">
+                    <ul class="projects-header__column__menu">
+                        <li><a href="#">Сначала новые</a><img class="projects-header__menu_arrow" src="../img/Arrow.png" alt="">
+                            <ul class="projects-header__column__submenu">
+                                <li><a href="#">Сначала старые</a></li>
+                                <li><a href="#">что-то ещё</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="projects-header__column__menu">
+                        <li><a href="#">Все проекты</a><img class="projects-header__menu_arrow" src="../img/Arrow.png" alt="">
+                            <ul class="projects-header__column__submenu">
+                                <li><a href="#">что-то ещё</a></li>
+                                <li><a href="#">что-то ещё</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </label>
+            </div>
+            <div class="projects-header__column">
+                <h1 class="projects__title">Наши проекты</h1>
+            </div>
+        </div>
+    </div>
+    <div class="projects__items">
+        <div class="section-item">
+            <div class="section-item-photo">
+                <img src="../img/foto.jpg" alt="">
+            </div>
+            <div class="section-item-hover">
+                <div class="section-item-subtitle">
+                    Название классификации проекта
+                </div>
+                <div class="section-item-title">
+                    Название выполненного проекта
+                </div>
+                <div class="section-item-row">
+                    <a href="#" class="item-but">Хочу также!</a>
+                    <a href="#" class="item-but2"><img src="img/arrow-right1.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="section-item">
+            <div class="section-item-photo">
+                <img src="../img/foto2.jpg" alt="">
+            </div>
+            <div class="section-item-hover">
+                <div class="section-item-subtitle">
+                    Название классификации проекта
+                </div>
+                <div class="section-item-title">
+                    Название выполненного проекта
+                </div>
+                <div class="section-item-row">
+                    <a href="#" class="item-but">Хочу также!</a>
+                    <a href="#" class="item-but2"><img src="img/arrow-right1.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="section-item">
+            <div class="section-item-photo">
+                <img src="../img/foto3.jpg" alt="">
+            </div>
+            <div class="section-item-hover">
+                <div class="section-item-subtitle">
+                    Название классификации проекта
+                </div>
+                <div class="section-item-title">
+                    Название выполненного проекта
+                </div>
+                <div class="section-item-row">
+                    <a href="#" class="item-but">Хочу также!</a>
+                    <a href="#" class="item-but2"><img src="img/arrow-right1.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="section-item">
+            <div class="section-item-photo">
+                <img src="../img/foto4.jpg" alt="">
+            </div>
+            <div class="section-item-hover">
+                <div class="section-item-subtitle">
+                    Название классификации проекта
+                </div>
+                <div class="section-item-title">
+                    Название выполненного проекта
+                </div>
+                <div class="section-item-row">
+                    <a href="#" class="item-but">Хочу также!</a>
+                    <a href="#" class="item-but2"><img src="img/arrow-right1.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="section-item">
+            <div class="section-item-photo">
+                <img src="../img/foto5.jpg" alt="">
+            </div>
+            <div class="section-item-hover">
+                <div class="section-item-subtitle">
+                    Название классификации проекта
+                </div>
+                <div class="section-item-title">
+                    Название выполненного проекта
+                </div>
+                <div class="section-item-row">
+                    <a href="#" class="item-but">Хочу также!</a>
+                    <a href="#" class="item-but2"><img src="img/arrow-right1.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="section-item">
+            <div class="section-item-photo">
+                <img src="../img/foto.jpg" alt="">
+            </div>
+            <div class="section-item-hover">
+                <div class="section-item-subtitle">
+                    Название классификации проекта
+                </div>
+                <div class="section-item-title">
+                    Название выполненного проекта
+                </div>
+                <div class="section-item-row">
+                    <a href="#" class="item-but">Хочу также!</a>
+                    <a href="#" class="item-but2"><img src="img/arrow-right1.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="section-item">
+            <div class="section-item-photo">
+                <img src="../img/foto7.jpg" alt="">
+            </div>
+            <div class="section-item-hover">
+                <div class="section-item-subtitle">
+                    Название классификации проекта
+                </div>
+                <div class="section-item-title">
+                    Название выполненного проекта
+                </div>
+                <div class="section-item-row">
+                    <a href="#" class="item-but">Хочу также!</a>
+                    <a href="#" class="item-but2"><img src="img/arrow-right1.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="section-item">
+            <div class="section-item-photo">
+                <img src="../img/foto8.jpg" alt="">
+            </div>
+            <div class="section-item-hover">
+                <div class="section-item-subtitle">
+                    Название классификации проекта
+                </div>
+                <div class="section-item-title">
+                    Название выполненного проекта
+                </div>
+                <div class="section-item-row">
+                    <a href="#" class="item-but">Хочу также!</a>
+                    <a href="#" class="item-but2"><img src="img/arrow-right1.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="section-item">
+            <div class="section-item-photo">
+                <img src="../img/foto9.jpg" alt="">
+            </div>
+            <div class="section-item-hover">
+                <div class="section-item-subtitle">
+                    Название классификации проекта
+                </div>
+                <div class="section-item-title">
+                    Название выполненного проекта
+                </div>
+                <div class="section-item-row">
+                    <a href="#" class="item-but">Хочу также!</a>
+                    <a href="#" class="item-but2"><img src="img/arrow-right1.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
