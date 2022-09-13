fb10f7340bf91ac109e0e85ef19f696679159acb
Alphison <alfismusin13@gmail.com>
2022-09-13 12:14:43 +0300

    add about us page

---
+++ pages/about_us.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700;800&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="about">
+        <div class="about__column">
+            <h1 class="about__title">
+                Немного о Elov Design
+            </h1>
+            <p class="about__subname">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Condimentum rutrum etiam tempor vel, tortor. Pharetra in nunc, in neque commodo convallis. Pellentesque nunc sed orci nullam pharetra magna. Nisi mattis id ut aenean velit est. Neque nulla ut turpis augue ac nulla.
+            </p>
+            <div class="about__dots">
+                <a href="https://vk.com/elovdesign" class="about__dot">
+                    <svg width="26" height="27" viewBox="0 0 26 27" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_20_28)">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M25.4042 7.05779C25.584 6.46629 25.4042 6.03079 24.5429 6.03079H21.6992C20.9755 6.03079 20.6418 6.40671 20.4609 6.82163C20.4609 6.82163 19.0147 10.284 16.9661 12.533C16.3031 13.1851 16.0019 13.392 15.6401 13.392C15.4592 13.392 15.1873 13.1851 15.1873 12.5925V7.05779C15.1873 6.34713 14.9879 6.03079 14.3856 6.03079H9.91358C9.46183 6.03079 9.18992 6.36013 9.18992 6.67321C9.18992 7.34596 10.2148 7.50196 10.3198 9.39563V13.5101C10.3198 14.4125 10.1541 14.5761 9.79225 14.5761C8.82808 14.5761 6.48267 11.0975 5.09058 7.11738C4.82083 6.34279 4.54783 6.03079 3.82092 6.03079H0.975C0.1625 6.03079 0 6.40671 0 6.82163C0 7.56046 0.964167 11.2308 4.49042 16.0852C6.84125 19.4002 10.1508 21.1975 13.1658 21.1975C14.9738 21.1975 15.197 20.7988 15.197 20.1109V17.6051C15.197 16.8067 15.3682 16.6475 15.9413 16.6475C16.3638 16.6475 17.0863 16.8555 18.7742 18.4534C20.7025 20.3481 21.0199 21.1975 22.1054 21.1975H24.9492C25.7617 21.1975 26.169 20.7988 25.935 20.0101C25.6772 19.2258 24.7563 18.0872 23.5354 16.7363C22.8724 15.9671 21.8779 15.1384 21.5757 14.7235C21.1543 14.1915 21.2745 13.9543 21.5757 13.4809C21.5757 13.4809 25.0423 8.68604 25.4031 7.05779H25.4042Z"/>
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_20_28">
+                                <rect width="26" height="26" transform="translate(0 0.614136)"/>
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </a>
+                <a href="https://vk.com/away.php?to=https%3A%2F%2Fwww.behance.net%2Felovdesign%2Fprojects" class="about__dot">
+                    <svg width="26" height="27" viewBox="0 0 26 27" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_20_32)">
+                            <path d="M23.8333 8.19746H16.25V6.03079H23.8333V8.19746ZM25.7032 19.0308C25.2243 20.4359 23.5051 22.2808 20.1771 22.2808C16.8469 22.2808 14.1494 20.4077 14.1494 16.1329C14.1494 11.897 16.6682 9.71954 20.0709 9.71954C23.4097 9.71954 25.4486 11.65 25.8938 14.5144C25.9783 15.0625 26.0119 15.8014 25.9967 16.8327H17.3008C17.4417 20.3113 21.0741 20.4207 22.2712 19.0308H25.7032ZM17.3767 14.6975H22.7554C22.6417 13.0215 21.5247 12.2935 20.072 12.2935C18.4838 12.2935 17.6052 13.1255 17.3767 14.6975ZM7.00483 22.2678H0V6.05354H7.53242C13.4647 6.14129 13.5774 11.9512 10.4791 13.535C14.2285 14.9 14.3542 22.2678 7.00483 22.2678V22.2678ZM3.25 12.5308H7.13267C9.84967 12.5308 10.2808 9.28079 6.79467 9.28079H3.25V12.5308ZM6.92358 15.7808H3.25V19.0481H6.86942C10.179 19.0481 9.97642 15.7808 6.92358 15.7808V15.7808Z"/>
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_20_32">
+                                <rect width="26" height="26" transform="translate(0 0.614136)"/>
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </a>
+                <a href="https://vk.com/away.php?to=https%3A%2F%2Fwww.behance.net%2Felovdesign%2Fprojects" class="about__dot">
+                    <svg width="26" height="28" viewBox="0 0 26 28" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_20_38)">
+                            <path d="M25.458 7.9345C25.3108 7.38914 25.024 6.89539 24.6282 6.50575C24.2212 6.10413 23.7223 5.81685 23.1787 5.671C21.1442 5.11413 12.9932 5.11413 12.9932 5.11413C9.59513 5.07398 6.19791 5.25056 2.82069 5.64288C2.27706 5.79949 1.77914 6.0932 1.37119 6.49788C0.970359 6.89838 0.680025 7.39225 0.528359 7.93338C0.164026 9.97159 -0.0129916 12.0411 -0.000307947 14.1141C-0.0133079 16.1853 0.163275 18.2541 0.528359 20.2949C0.676775 20.8338 0.966025 21.3254 1.36794 21.7225C1.76986 22.1196 2.27036 22.4065 2.82069 22.5584C4.88228 23.1141 12.9932 23.1141 12.9932 23.1141C16.3956 23.1543 19.7971 22.9778 23.1787 22.5854C23.7223 22.4395 24.2212 22.1523 24.6282 21.7506C25.0239 21.3611 25.3103 20.8673 25.4569 20.3219C25.8308 18.2845 26.0126 16.214 25.9997 14.14C26.0278 12.0572 25.8463 9.97701 25.458 7.93338V7.9345ZM10.4019 17.9661V10.2633L17.1835 14.1153L10.4019 17.9661Z"/>
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_20_38">
+                                <rect width="26" height="27" transform="translate(0 0.614136)"/>
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </a>
+            </div>
+        </div>
+        <div class="about__column">
+            <img src="../img/foto_about.png" alt="">
+        </div>
+    </div>
+</body>
+</html>
