f5fff3665c7bd35c4a009707b30598fa0f0383c1
Alphison <alfismusin13@gmail.com>
2022-09-17 18:46:36 +0300

    add project

---
+++ pages/project.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="header__inner">
+            <div class="logo">
+                <img src="../img/logo.png" alt="">
+            </div>
+            <nuv class="menu-header">
+                <ul>
+                    <li><a href="#">Проекты</a></li>
+                    <li><a href="#">О нас</a></li>
+                    <li><a href="#">Хочу дизайн</a></li>
+                </ul>
+            </nuv>
+        </div>
+    </header>
+    <div class="project-banner">
+        <img src="../img/banner.jpg" alt="">
+    </div>
+    <div class="project-wrapper">
+        <p class="project-subname">
+            Классификация преокта
+        </p>
+        <h1 class="project-name">
+            Название проекта
+        </h1>
+        <p class="task">
+            Задача
+        </p>
+        <p class="task-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sit tellus viverra non. Eget egestas purus, neque dolor convallis. Risus arcu sed porta orci. Amet gravida aliquam rutrum massa. Enim lectus sed pharetra pretium sit. Enim dui in in augue integer tortor molestie ipsum. Gravida mattis nunc nulla magna sodales adipiscing euismod. Erat arcu nibh hac dis et eget porta vulputate. Sed mus sodales ultricies duis tortor nulla viverra at.<br><br>
+            Libero id hac sed luctus. Condimentum euismod aenean sem lectus augue imperdiet lacinia quis. Viverra dolor, fermentum, molestie duis pretium parturient morbi eu nunc. Leo velit velit id enim tincidunt blandit amet, consectetur amet. Sed scelerisque eget faucibus nam augue at.
+            Amet dignissim pretium sagittis, justo, quis. Eget ut liber
+        </p>
+        <div class="project-img">
+            <img src="../img/banner2.jpg" alt="">
+        </div>
+        <p class="project-img__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sit tellus viverra non. Eget egestas purus, neque dolor convallis. Risus arcu sed porta orci. Amet gravida aliquam rutrum massa. Enim lectus sed pha
+        </p>
+        <div class="project-row">
+            <div class="project-column">
+                <label for="" class="project-info">
+                    <p class="project-info__name">
+                        Дата публикации:
+                    </p>
+                    <div class="project-info__text">
+                        <p>17.09.2022</p>
+                    </div>
+                </label>
+            </div>
+            <div class="project-column">
+                <label for="" class="project-info">
+                    <p class="project-info__name">
+                        Ещё что-то:
+                    </p>
+                    <div class="project-info__text">
+                        <p>Имя Фамилия</p>
+                        <p>Имя Фамилия</p>
+                    </div>
+                </label>
+                <label for="" class="project-info">
+                    <p class="project-info__name">
+                        Дизайнеры:
+                    </p>
+                    <div class="project-info__text">
+                        <p>Имя Фамилия</p>
+                        <p>Имя Фамилия</p>
+                    </div>
+                </label>
+                <label for="" class="project-info">
+                    <p class="project-info__name">
+                        Верстальщики:
+                    </p>
+                    <div class="project-info__text">
+                        <p>Имя Фамилия</p>
+                        <p>Имя Фамилия</p>
+                    </div>
+                </label>
+                <label for="" class="project-info">
+                    <p class="project-info__name">
+                        Арт-Директор
+                    </p>
+                    <div class="project-info__text">
+                        <p>Имя Фамилия</p>
+                    </div>
+                </label>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
